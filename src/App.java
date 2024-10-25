d173e6953c57ec26c093edf604091045b572f830
calebbrunkhart <caleb.brunkhart@umontana.edu>
2024-10-25 15:37:38 -0600

    captures work. Scoring technically works, but not really for a complex board state. Very basic scoring mechanic. Breaks if one different colored piece is in the other color's territory

--- src/App.java
+++ src/App.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
 
 public class App {
 
     static String[][] goBoard = {
-        {null, null, " o", " o", null, null, null, null, null},
-        {null, " o", " x", " x", " o", null, null, null, null},
-        {null, " o", " x", null, " x", " o", null, null, null},
-        {null, " o", " x", " x", " x", " o", null, null, null},
-        {null, " o", " x", null, " x", " o", null, null, null},
-        {null, null, " o", " x", " x", " o", null, null, null},
-        {null, null, null, " o", " o", null, null, null, null},
-        {null, null, null, null, null, null, null, null, null},
-        {null, null, null, null, null, null, null, null, null}
-    };
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", null, null, null, null, null},
+        {null, null, null, " x", " o", " o", " o", " o", " o"},
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", " o", null, null, null, null},
+        {null, null, null, " x", " o", null, null, null, null}
+        };
 
-
-    static boolean[][] lives = new boolean[9][9];
     static boolean[][] territory = new boolean[9][9];
     static boolean[][] beenChecked = new boolean[9][9];
 
-// *This prints the board to the terminal
+//This prints the board to the terminal
     static void getBoard(String[][] b){
         System.out.println("  0 1 2 3 4 5 6 7 8");
         for (int i = 0; i < b.length; i++){
@@ -35,6 +35,7 @@ public class App {
         }
     }
 
+//Checking if a space has liberties
     static boolean hasLiberties(int x, int y) {
         if(goBoard[x][y] == null) return true;
         if(x+1 <= 8 && goBoard[x+1][y] == null) return true;
@@ -84,13 +85,49 @@ public class App {
 
     }
 
+    static int checkTerritory(int x, int y, Set<String> borderingColors) {
+        if (x < 0 || x > 8 || y < 0 || y > 8 || goBoard[x][y] != null || territory[x][y]) {
+            return 0;
+        }
+
+        territory[x][y] = true;
+        int areaCount = 1; // Initialize territory count
+
+        // Check adjacent spaces and expand the territory
+        if (x + 1 <= 8) {
+            if (goBoard[x + 1][y] != null) borderingColors.add(goBoard[x + 1][y]);
+            else areaCount += checkTerritory(x + 1, y, borderingColors);
+        }
+        if (x - 1 >= 0) {
+            if (goBoard[x - 1][y] != null) borderingColors.add(goBoard[x - 1][y]);
+            else areaCount += checkTerritory(x - 1, y, borderingColors);
+        }
+        if (y + 1 <= 8) {
+            if (goBoard[x][y + 1] != null) borderingColors.add(goBoard[x][y + 1]);
+            else areaCount += checkTerritory(x, y + 1, borderingColors);
+        }
+        if (y - 1 >= 0) {
+            if (goBoard[x][y - 1] != null) borderingColors.add(goBoard[x][y - 1]);
+            else areaCount += checkTerritory(x, y - 1, borderingColors);
+        }
+
+        return areaCount;
+    }
 
     public static void main(String[] args) throws Exception {
 
         Scanner scn = new Scanner(System.in);
         Boolean quit = false;
         int moveX, moveY;
-        Boolean flipper = true; 
+        Boolean flipper = true;
+
+    //Values to track whether the game has ended. If both players pass in succession, the game ends.
+        String pass; 
+        int pass_count = 0;
+
+    //White "O" starts with .5 points to ensure there is a winner. Black has an advantage of going first, so white gets the half point. This is called Komi.
+        int playerXscore = 0;
+        double playerOscore = 0.5;
 
         getBoard(goBoard);
 
@@ -98,8 +135,30 @@ public class App {
             System.out.println();
 
             boolean[][] dead = new boolean [9][9];
+            
+            if (pass_count == 2) {
+                quit = true;
+                continue;
+            }
+
+            System.out.println("Would you like to pass? (y for yes, n for no)");
+            pass = scn.next();
+            if (pass.equalsIgnoreCase("y")) {
+                pass_count++;
+                flipper = !flipper;
+                continue;                    
+            }
+            else pass_count = 0;
+ 
 
             while (true){
+                System.out.println(pass_count);
+                if (flipper) {
+                    System.out.println("Player 1's turn");
+                }
+                else {
+                    System.out.println("Player 2's turn");
+                }
                 System.out.println();
                 System.out.println("Please enter the x coordinate for your piece (1-9): ");
                 moveX = scn.nextInt();
@@ -136,6 +195,12 @@ public class App {
                 for (int j = 0; j < goBoard[i].length; j++){
                     if (dead[i][j]) {
                         goBoard[i][j] = null;
+                        if (flipper) {
+                            playerXscore++;
+                        }
+                        else {
+                            playerOscore++;
+                        }
                     }
                 }
             }
@@ -149,9 +214,30 @@ public class App {
                 flipper = !flipper;
             }
         }
+
+        territory = new boolean[9][9];
+        for (int i = 0; i < goBoard.length; i++) {
+            for (int j = 0; j < goBoard[i].length; j++) {
+                if (goBoard[i][j] == null && !territory[i][j]) {
+                    Set<String> borderingColors = new HashSet<>();
+                    int areaSize = checkTerritory(i, j, borderingColors);
+                    if (borderingColors.size() == 1) {
+                        String color = borderingColors.iterator().next();
+                        if (color.equals(" x")) {
+                            playerXscore += areaSize;
+                        }
+                        else {
+                            playerOscore += areaSize;
+                        }
+                    }
+                }
+            }
+        } 
+
+        System.out.println("Player 1's score: " + playerXscore);
+        System.out.println("Player 2's score: " + playerOscore);
         scn.close();
     }
 
 }
-
 
